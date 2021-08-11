7b67e1139d7bbc2cab2167e0812d58056cb51eb3
konstantinosKokos <konstantinos@riseup.net>
2021-08-11 14:47:22 +0200

    update lexicon and add data folder

--- src/examples/nl_nl/lexicon/lexicon.py
+++ src/examples/nl_nl/lexicon/lexicon.py
@@ -1,23 +1,25 @@
+from src import cwdir
 import os
 
-de_noun_fn = os.path.join('./data/processed/de_personen.txt')
-het_noun_fn = os.path.join('./data/processed/het_personen.txt')
+data_dir = os.path.join(cwdir, 'examples/nl_nl/data/')
+
+de_noun_fn = os.path.join(data_dir, 'de_personen.txt')
+het_noun_fn = os.path.join(data_dir, 'het_personen.txt')
+obj_cv_fn = os.path.join(data_dir, 'obj_control_verbs.txt')
+sub_cv_fn = os.path.join(data_dir, 'sub_control_verbs.txt')
+inf_fn = os.path.join(data_dir, 'infinitives.txt')
+
 de_nouns = [ln.strip() for ln in open(de_noun_fn, 'r').readlines()]
 het_nouns = [ln.strip() for ln in open(het_noun_fn, 'r').readlines()]
 
-subj_control_verbs_present_tense = ['belooft', 'meldt', 'zegt', 'garandeert', 'vertelt', 'bezweert', 'antwoordt', 'verzekert', 'zweert']
-subj_control_verbs_inf = ['beloven', 'melden', 'zeggen', 'garanderen', 'vertellen', 'bezweren', 'antwoorden',
-                          'verzekeren', 'zweren']
 
-obj_control_verbs_present_tense = ['smeekt', 'helpt', 'sommeert', 'instrueert', 'gebaart', 'ontraadt', 'traint', 'vraagt', 'overtuigt',
-                                   'gunt', 'verwijt', 'motiveert', 'machtigt', 'dwingt', 'verplicht', 'commandeert', 'beweegt', 'beveelt',
-                                   'prikkelt', 'verbiedt', 'verhindert', 'verleidt', 'belet', 'gelast', 'gebiedt', 'overreedt', 'stimuleert',
-                                   'waarschuwt', 'leert', 'suggereert', 'verzoekt', 'adviseert', 'belemmert']
-obj_control_verbs_inf = ['smeken', 'helpen', 'sommeren', 'instrueren', 'gebaren', 'ontraden', 'trainen', 'vragen',
-                         'overtuigen', 'gunnen', 'verwijten', 'motiveren', 'machtigen',  'dwingen', 'verplichten',
-                         'commanderen', 'bewegen', 'bevelen', 'prikkelen', 'overhalen', 'verbieden', 'verhinderen',
-                         'verleiden', 'beletten', 'gelasten' 'gebieden', 'overreden', 'stimuleren', 'waarschuwen',
-                         'leren', 'suggereren', 'verzoeken', 'adviseren', 'belemmeren']
+def load_control_verbs(fn: str) -> tuple[list[str], list[str]]:
+    with open(fn, 'r') as infile:
+        verbs_present, verbs_inf = zip(*[ln.strip().split('\t') for ln in infile.readlines()])
+    return verbs_present, verbs_inf
 
-infinitive_verbs = ['vertrekken', 'gaan', 'komen', 'winnen', 'verliezen', 'praten', 'stoppen', 'beginnen', 'kijken', 'stemmen']
+
+obj_control_verbs_present, obj_control_verbs_inf = load_control_verbs(obj_cv_fn)
+sub_control_verbs_present, sub_control_verbs_inf = load_control_verbs(sub_cv_fn)
+infinitive_verbs = [ln.strip() for ln in open(inf_fn).readlines()]
 
